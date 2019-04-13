cc7045d443ea41d997f0a9126fc10782f9cee834
Caleb Robinson <calebrob6@gmail.com>
2019-04-12 17:27:23 -0700

    0.  Adding migration data

---
+++ MigrationData.py
@@ -0,0 +1,207 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Caleb Robinson <calebrob6@gmail.com>
+#
+# Distributed under terms of the MIT license.
+import os
+import csv
+
+def parse_row_04_08(line):
+    ''' This method parses rows from the 2004-2008 IRS migration data.
+    
+    This data is in a fixed width data format where column names are like the following:
+    State_Code_Origin, County_Code_Origin, State_Code_Dest, County_Code_Dest, State_Abbrv, County_Name, Return_Num, Exmpt_Num, Aggr_AGI
+    '''
+    row = dict()
+
+    row['State_Code_Origin'] = line[0:2].strip()
+    row['County_Code_Origin'] = line[3:6].strip()
+    row['State_Code_Dest'] = line[7:9].strip()
+    row['County_Code_Dest'] = line[10:13].strip()
+    row['State_Abbrv'] = line[14:16].strip()
+    row['County_Name'] = line[17:49].strip()
+    row['Return_Num'] = line[50:59].strip()
+    row['Exmpt_Num'] = line[60:70].strip()
+    row['Aggr_AGI'] = line[71:82].strip()
+    
+    return row
+
+def load_file_04_08(fn):
+    ''' Method to load 2004-2008 IRS migration data from file.
+    '''
+    origin_set = set()
+    destination_set = set()
+
+    records = []
+
+    f = open(fn,'r')
+    for line in f:
+        line = line.strip()
+        if line!='':
+            row = parse_row_04_08(line)
+
+            val = int(row['Exmpt_Num'])
+
+            origin = '%02d%03d' % (int(row['State_Code_Origin']), int(row['County_Code_Origin']))
+            destination = '%02d%03d' % (int(row['State_Code_Dest']), int(row['County_Code_Dest']))
+
+            origin_set.add(origin)
+            destination_set.add(destination)
+            records.append((origin,destination,val))
+    f.close()
+
+    return records
+
+def load_file_08_11(fn):
+    ''' Method to load 2008-2011 IRS migration data from file.
+    '''
+    origin_set = set()
+    destination_set = set()
+
+    records = []
+
+    f = open(fn,'r')
+    reader = csv.DictReader(f)
+    for row in reader:
+        val = int(row['Exmpt_Num'])
+
+        origin = '%02d%03d' % (int(row['State_Code_Origin']), int(row['County_Code_Origin']))
+        destination = '%02d%03d' % (int(row['State_Code_Dest']), int(row['County_Code_Dest']))
+
+        origin_set.add(origin)
+        destination_set.add(destination)
+        records.append((origin,destination,val))
+    f.close()
+
+    return records
+
+
+def load_file_11_15(fn):
+    ''' Method to load 2011-2015 IRS migration data from file.
+    '''
+    origin_set = set()
+    destination_set = set()
+
+    records = []
+
+    f = open(fn,'r')
+    reader = csv.DictReader(f)
+    for row in reader:
+        val = int(row['n2'])
+
+        origin = '%02d%03d' % (int(row['y1_statefips']), int(row['y1_countyfips']))
+        destination = '%02d%03d' % (int(row['y2_statefips']), int(row['y2_countyfips']))
+
+        origin_set.add(origin)
+        destination_set.add(destination)
+        records.append((origin,destination,val))
+    f.close()
+
+    return records
+
+class IRSMigrationData(object):
+
+    YEAR_FN_MAP = {
+        # year : (dirname, infilename, outfilename),
+        2004 : ('county0405','countyin0405us1.dat','countyout0405us1.dat'),
+        2005 : ('county0506','countyin0506.dat','countyout0506.dat'),
+        2006 : ('county0607','countyin0607.dat','countyout0607.dat'),
+        2007 : ('county0708','ci0708us.dat','co0708us.dat'),
+        2008 : ('county0809','countyinflow0809.csv','countyoutflow0809.csv'),
+        2009 : ('county0910','countyinflow0910.csv','countyoutflow0910.csv'),
+        2010 : ('county1011','countyinflow1011.csv','countyoutflow1011.csv'),
+        2011 : ('county1112','countyinflow1112.csv','countyoutflow1112.csv'),
+        2012 : ('county1213','countyinflow1213.csv','countyoutflow1213.csv'),
+        2013 : ('county1314','countyinflow1314.csv','countyoutflow1314.csv'),
+        2014 : ('county1415','countyinflow1415.csv','countyoutflow1415.csv'),
+    }
+
+    def __init__(self, data_dir='data/raw/migration/'):
+        self.data_dir = data_dir
+    
+    def get_fn_from_year(self, year):
+        '''Get paths to the IRS data assosciated with some year.
+
+        Input: year - the year of data to get as an int
+        Output: incoming_fn, outgoing_fn - absolute paths to the inflow and outflow data files 
+        '''
+        if year in IRSMigrationData.YEAR_FN_MAP:
+            incoming_fn = os.path.join(self.data_dir, IRSMigrationData.YEAR_FN_MAP[year][0], IRSMigrationData.YEAR_FN_MAP[year][1])
+            outgoing_fn = os.path.join(self.data_dir, IRSMigrationData.YEAR_FN_MAP[year][0], IRSMigrationData.YEAR_FN_MAP[year][2])
+
+            return incoming_fn, outgoing_fn
+        else:
+            raise ValueError('Year %d out of range' % (year))
+
+    def get_raw_data(self, year):
+        ''' Get rows from raw IRS migration data files.
+
+        Input: year - the year of data to get as an int
+        Output: list incoming and outgoing migrant records from that year in the format (origin, destination, number of exemptions)
+        '''
+        incoming_fn, outgoing_fn = self.get_fn_from_year(year)
+
+        in_records = None
+        out_records = None
+        if 2004<=year<2008:
+            in_records = load_file_04_08(incoming_fn)
+            out_records = loadFile04_08(outgoing_fn)
+        elif 2008<=year<2011: 
+            in_records = load_file_08_11(incoming_fn)
+            out_records = load_file_08_11(outgoing_fn)
+        elif 2011<=year<2015:
+            in_records = load_file_11_15(incoming_fn)
+            out_records = load_file_11_15(outgoing_fn)
+        else:
+            raise ValueError('Year %d out of range' % (year))
+
+        return in_records, out_records
+
+    def get_processed_data(self, year, county_fips, verbose=False):
+        ''' Get migration matrix for a given year and set of county FIPS codes.
+
+        Input: year - the year of data to get as an int
+        Output: Migration matrix of size (|county_fips| x |county_fips|) where an i,j entry corresponds to the number of migrants leaving county i for county j
+        '''
+    
+        county_set = set(county_fips)
+        assert len(county_set) == len(county_fips), 'List of counties should not have duplicates'
+        county_fips_to_idx = {fips:i for i, fips in enumerate(county_fips)}
+        n = len(county_fips)
+
+        migration_matrix = np.zeros((n,n), dtype=np.uint16)
+        
+        in_records, out_records = self.get_raw_data(year)
+
+        for origin, destination, val in in_records:
+            if 2004<=year<2008: # the 2004 - 2008 data has this backwards
+                origin, destination = destination, origin
+            
+            origin_idx = county_fips_to_idx[origin] 
+            destination_idx = county_fips_to_idx[destination]
+
+            if origin in county_set and destination in county_set:
+                migration_matrix[origin_idx, destination_idx] = val
+
+        repeats = 0
+        discrepancies = 0
+        for origin, destination, val in out_records:
+            if origin in county_set and destination in county_set:
+                origin_idx = county_fips_to_idx[origin] 
+                destination_idx = county_fips_to_idx[destination]
+
+                if migration_matrix[origin_idx, destination_idx] != 0:
+                    if migration_matrix[origin_idx, destination_idx] != val:
+                        repeats += 1
+                        discrepancies += abs(migration_matrix[origin_idx, destination_idx] - val)
+
+                migration_matrix[origin_idx, destination_idx] = val
+        
+        # Sanity check, there should not be any u,v in the in_records that contradict with records from out_records
+        if verbose:
+            print("Found %d repeats" % (repeats))
+            print("Error of %d migrants" % (discrepancies))
+
+        return migration_matrix
---
+++ MigrationModels.py
@@ -0,0 +1,196 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 Caleb Robinson <calebrob6@gmail.com>
+#
+# Distributed under terms of the MIT license.
+'''
+Vectorized (fast) implementation of the radiation model[1], the extended radiation model[2], and the gravity model with power and exponential law decay[3].
+
+All implementations have a `slowMode` flag which, when true, calculates the results using a nested for loop (instead of in a vectorized manner).
+We inlcude tests that assert the results of the vectorized implementation against the "slowMode" implementations.
+
+[1] Simini, Filippo, et al. "A universal model for mobility and migration patterns." Nature 484.7392 (2012): 96-100.
+[2] Yang, Yingxiang, et al. "Limits of Predictability in Commuting Flows in the Absence of Data for Calibration." Scientific Reports 4 (2014).
+[3] Lenormand, Maxime, Aleix Bassolas, and José J. Ramasco. "Systematic comparison of trip distribution laws and models." Journal of Transport Geography 51 (2016): 158-169.
+'''
+import sys
+import os
+import time
+
+import numpy as np
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+# Misc methods
+#-----------------------------------------------------------------------------------------------------------------------------------
+def productionFunction(population, P, beta=0.03):
+    return P * (population*beta)
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------
+def extendedRadiationModel(origins, destinations, s, alpha, rowNormalize=True, slowMode=False):
+    assert len(origins.shape) == 2 and origins.shape[1] == 1, "`origins` and `destinations` must be 2D with a single column"
+    assert len(destinations.shape) == 2 and destinations.shape[1] == 1, "`origins` and `destinations` must be 2D with a single column"
+    assert origins.shape[0] == destinations.shape[0], "`origins` and `destinations` must be the same shape"
+
+    n = origins.shape[0]
+
+    assert len(s.shape) == 2 and s.shape[0] == n and s.shape[1] == n, "`s` must be a square matrix with same length/width as the origin and destination features"
+
+    origins = origins.astype(np.float)
+    P = np.zeros((n,n), dtype=float)
+
+    if slowMode:
+        for i in range(n):
+            for j in range(n):
+                numerator = ((origins[i] + destinations[j] + s[i,j])**alpha - (origins[i] + s[i,j])**alpha) * (origins[i]**alpha + 1)
+                denominator = ((origins[i] + s[i,j])**alpha + 1) * ((origins[i] + destinations[j] + s[i,j])**alpha + 1)
+                P[i,j] = numerator/denominator
+    else:
+        numerator = ((s + origins + destinations.T)**alpha - (s + origins)**alpha) * (origins**alpha + 1)
+        denominator = ((s+origins)**alpha + 1) * ((s + origins  + destinations.T)**alpha + 1)
+        P = np.divide(numerator,denominator)
+
+    np.fill_diagonal(P, 0.0)
+
+    # do row normalization, i.e. make each row sum to 1
+    if rowNormalize: 
+        rowSums = np.sum(P,axis=1).reshape(-1,1)
+        P = P/rowSums
+
+    P[np.isnan(P) | np.isinf(P)] = 0.0
+    assert not np.any(np.isnan(P))
+    assert not np.any(np.isinf(P))
+
+    return P
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------
+def radiationModel(origins, destinations, s, rowNormalize=True, slowMode=False):
+    assert len(origins.shape) == 2 and origins.shape[1] == 1, "`origins` and `destinations` must be 2D with a single column"
+    assert len(destinations.shape) == 2 and destinations.shape[1] == 1, "`origins` and `destinations` must be 2D with a single column"
+    assert origins.shape[0] == destinations.shape[0], "`origins` and `destinations` must be the same shape"
+
+    n = origins.shape[0]
+
+    assert len(s.shape) == 2 and s.shape[0] == n and s.shape[1] == n, "`s` must be a square matrix with same length/width as the origin and destination features"
+
+    origins = origins.astype(np.float)
+    P = np.zeros((n,n), dtype=float)
+
+    if slowMode:
+        for i in range(n):
+            for j in range(n):
+                P[i,j] = (origins[i] * destinations[j]) / ((s[i,j] + origins[i]) * (s[i,j] + origins[i] + destinations[j]))
+    else:
+        numerator = np.dot(origins,destinations.T)
+        denominator = (s + origins) * (s + origins + destinations.T) 
+        
+        P = np.divide(numerator,denominator)
+
+    np.fill_diagonal(P, 0.0)
+
+    # do row normalization, i.e. make each row sum to 1
+    if rowNormalize:
+        rowSums = np.sum(P,axis=1).reshape(-1,1)
+        P = P/rowSums
+
+    P[np.isnan(P) | np.isinf(P)] = 0.0
+    assert not np.any(np.isnan(P))
+    assert not np.any(np.isinf(P))
+
+    return P
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------
+def gravityModel(origins, destinations, d, alpha, decay="power", rowNormalize=True, slowMode=False):
+    assert len(origins.shape) == 2 and origins.shape[1] == 1, "`origins` and `destinations` must be 2D with a single column"
+    assert len(destinations.shape) == 2 and destinations.shape[1] == 1, "`origins` and `destinations` must be 2D with a single column"
+    assert origins.shape[0] == destinations.shape[0], "`origins` and `destinations` must be the same shape"
+
+    n = origins.shape[0]
+
+    assert len(d.shape) == 2 and d.shape[0] == n and d.shape[1] == n, "`d` must be a square matrix with same length/width as the origin and destination features"
+
+    assert decay in ["power", "exponential"], "`decay` must be either 'power' or 'exponential'"
+
+    origins = origins.astype(np.float)
+    d = d.astype(np.float)
+
+    P = np.zeros((n,n), dtype=float)
+
+    if slowMode:
+        for i in range(n):
+            for j in range(n):
+                if i!=j: # on the diagonal the distance matrix will be zero, so we ignore these
+                    if decay=="power":
+                        P[i,j] = (origins[i] * destinations[j]) / (d[i,j]**alpha)
+                    elif decay=="exponential":
+                        P[i,j] = (origins[i] * destinations[j]) / (np.exp(d[i,j]*alpha))                
+    else:
+        numerator = np.dot(origins,destinations.T)
+        denominator = None
+        if decay=="power":
+            denominator = d**(alpha)
+        elif decay=="exponential":
+            denominator = np.exp(d*alpha)
+        # the denominator will be 0 where d[i,j] == 0, causing a divide by zero error 
+        mask = denominator==0 # record where there are 0's
+        denominator[mask]=1.0 # set the 0's to 1.0, avoiding the divide by 0
+        P = np.divide(numerator,denominator)
+        P[mask] = 0.0 # set the results that would have been divided by 0, to 0
+
+    np.fill_diagonal(P, 0.0)
+
+     # do row normalization, i.e. make each row sum to 1
+    if rowNormalize:
+        rowSums = np.sum(P,axis=1).reshape(-1,1)
+        P = P/rowSums
+
+    P[np.isnan(P) | np.isinf(P)] = 0.0
+    assert not np.any(np.isnan(P))
+    assert not np.any(np.isinf(P))
+
+    return P
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------------------
+def getInterveningOpportunities(features, distanceMatrix, slowMode=False):
+    assert len(features.shape) == 2
+    assert features.shape[1] == 1
+
+    n = features.shape[0]
+
+    assert len(distanceMatrix.shape)==2
+    assert distanceMatrix.shape[0] == distanceMatrix.shape[1]
+    assert distanceMatrix.shape[0] == n
+
+    S = np.zeros((n, n), dtype=np.float32)
+    
+    if slowMode:
+        # Naively iterate over all locations, sort distances to others, assign intervening opportunities
+        for i in range(n):
+            otherPatches = []
+            for j in range(n):
+                otherPatches.append((distanceMatrix[i,j], j))
+            otherPatches.sort(key=lambda x: x[0]) # this is a stable sort
+
+            cumSum = 0.0
+            for distance, j in otherPatches[1:]:
+                S[i,j] = cumSum
+                cumSum += features[j]
+    else:
+        # Very important that we use mergesort here as it is a stable sort
+        d = np.argsort(distanceMatrix, kind="mergesort", axis=1)
+
+        for i in range(n):
+            tempFeatures = features[d[i]]
+            newFeatures = [0,0] + list(np.cumsum(tempFeatures[1:-1]))
+            S[i,d[i]] =  newFeatures
+
+    return S
+
+
+if __name__ == "__main__":
+    pass
